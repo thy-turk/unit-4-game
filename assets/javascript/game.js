0174020cac2b1d6aa53a289f97596c38c92234d9
Turk <tuckerfiumara@gmail.com>
2019-10-26 15:44:35 -0700

    Did some cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,26 +8,6 @@ var losses= 0;
 var totalScore = 0;
 
 
-
-// var randomNumber = function randomNumberGen() {  
-//     return Math.floor(Math.random() * 102) + 19; 
-// }
-
-// var gem1Val = function randomNumberGem1() {  
-//     return Math.floor(Math.random() * 12) + 1; 
-// }
-
-// var randomNumber = Math.floor(Math.random() * 102) + 19;
-
-// var gem1Val = Math.floor(Math.random() * 12) + 1;
-
-// var gem2Val = Math.floor(Math.random() * 12) + 1;
-
-// var gem3Val = Math.floor(Math.random() * 12) + 1;
-
-// var gem4Val = Math.floor(Math.random() * 12) + 1;
-
-
 function gameStart() {
     randomNumber = Math.floor(Math.random() * 102) + 19;
 
@@ -46,41 +26,26 @@ function gameStart() {
     $('#total-score').text(totalScore);
 }
 
+function winOrLose() {
+    if (totalScore === randomNumber) {
+        wins++;
+        $('#wins').text(wins);
+        gameStart();
+    } else if (totalScore > randomNumber) {
+        losses++;
+        $('#losses').text(losses);
+        gameStart();
+    } else {
+
+    }
+
+}
+
+$('#wins').text(wins);
+$('#losses').text(losses);
 
 gameStart();
 
-
-// $('#random-number').text(randomNumber);
-
-// $('#wins').text(wins);
-
-// $('#losses').text(losses);
-
-// $('#total-score').text(totalScore);
-
-
-
-// function winOrLose() {
-//     if (totalScore.val === randomNumber.val) {
-//         winner();
-//     } else if (totalScore.val > randomNumber.val) {
-//         loser();
-//     } else {
-
-//     }
-// }
-
-// function winner() {
-//     wins++;
-//     $('#wins').text(wins);
-//     gameStart();
-// }
-
-// function loser() {
-//     losses++;
-//     $('#losses').text(losses);
-//     gameStart();
-// }
 
 $('#gem1').on("click", function() {
    totalScore = totalScore + gem1Val;
@@ -106,17 +71,3 @@ $('#gem3').on("click", function() {
      winOrLose();
  });
 
- function winOrLose() {
- if (totalScore === randomNumber) {
-    wins++;
-    $('#wins').text(wins);
-    gameStart();
-} else if (totalScore > randomNumber) {
-    losses++;
-    $('#losses').text(losses);
-    gameStart();
-} else {
-
-}
-
-}
