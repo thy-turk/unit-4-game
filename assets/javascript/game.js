b0e9179878dc2285eeff050d5a96521875355b67
Turk <tuckerfiumara@gmail.com>
2019-10-26 15:35:51 -0700

    Finished JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,69 +9,78 @@ var totalScore = 0;
 
 
 
-var randomNumber = function randomNumberGen() {  
-    return Math.floor(Math.random() * 102) + 19; 
-}
+// var randomNumber = function randomNumberGen() {  
+//     return Math.floor(Math.random() * 102) + 19; 
+// }
 
 // var gem1Val = function randomNumberGem1() {  
 //     return Math.floor(Math.random() * 12) + 1; 
 // }
 
-var gem1Val = Math.floor(Math.random() * 12) + 1;
+// var randomNumber = Math.floor(Math.random() * 102) + 19;
 
-var gem2Val = Math.floor(Math.random() * 12) + 1;
+// var gem1Val = Math.floor(Math.random() * 12) + 1;
 
-var gem3Val = Math.floor(Math.random() * 12) + 1;
+// var gem2Val = Math.floor(Math.random() * 12) + 1;
 
-var gem4Val = Math.floor(Math.random() * 12) + 1;
+// var gem3Val = Math.floor(Math.random() * 12) + 1;
+
+// var gem4Val = Math.floor(Math.random() * 12) + 1;
 
 
 function gameStart() {
-    randomNumberGen();
-    var gem1Val = Math.floor(Math.random() * 12) + 1;
+    randomNumber = Math.floor(Math.random() * 102) + 19;
 
-    var gem2Val = Math.floor(Math.random() * 12) + 1;
+    $('#random-number').text(randomNumber);
 
-    var gem3Val = Math.floor(Math.random() * 12) + 1;
+    gem1Val = Math.floor(Math.random() * 12) + 1;
 
-    var gem4Val = Math.floor(Math.random() * 12) + 1;
+    gem2Val = Math.floor(Math.random() * 12) + 1;
+
+    gem3Val = Math.floor(Math.random() * 12) + 1;
+
+    gem4Val = Math.floor(Math.random() * 12) + 1;
 
     totalScore = 0;
+
+    $('#total-score').text(totalScore);
 }
 
 
+gameStart();
+
+
+// $('#random-number').text(randomNumber);
+
+// $('#wins').text(wins);
+
+// $('#losses').text(losses);
+
+// $('#total-score').text(totalScore);
 
 
 
-$('#random-number').text(randomNumber);
+// function winOrLose() {
+//     if (totalScore.val === randomNumber.val) {
+//         winner();
+//     } else if (totalScore.val > randomNumber.val) {
+//         loser();
+//     } else {
 
-$('#wins').text(wins);
+//     }
+// }
 
-$('#losses').text(losses);
+// function winner() {
+//     wins++;
+//     $('#wins').text(wins);
+//     gameStart();
+// }
 
-$('#total-score').text(totalScore);
-
-function winOrLose() {
-    if (totalScore == randomNumber) {
-        winner();
-    } else if (totalScore > randomNumber) {
-        loser();
-    } else {
-
-    }
-}
-
-function winner() {
-    wins++;
-    $('#wins').text(wins);
-    gameStart();
-}
-
-function loser() {
-    losses++;
-    $('#losses').text(losses);
-    gameStart();
-}
+// function loser() {
+//     losses++;
+//     $('#losses').text(losses);
+//     gameStart();
+// }
 
 $('#gem1').on("click", function() {
    totalScore = totalScore + gem1Val;
@@ -85,3 +94,29 @@ $('#gem2').on("click", function() {
     winOrLose();
 });
 
+$('#gem3').on("click", function() {
+    totalScore = totalScore + gem3Val;
+    $('#total-score').text(totalScore);
+    winOrLose();
+ });
+ 
+ $('#gem4').on("click", function() {
+     totalScore = totalScore + gem4Val;
+     $('#total-score').text(totalScore);
+     winOrLose();
+ });
+
+ function winOrLose() {
+ if (totalScore === randomNumber) {
+    wins++;
+    $('#wins').text(wins);
+    gameStart();
+} else if (totalScore > randomNumber) {
+    losses++;
+    $('#losses').text(losses);
+    gameStart();
+} else {
+
+}
+
+}
